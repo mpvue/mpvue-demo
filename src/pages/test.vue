60fdaba81c5c8a56837f7d34674f23c9169e0e17
xuewenfei <steven.xue@alo7.com>
2018-09-12 18:36:35 +0800

    研究mpvue的生命周期

---
+++ src/pages/test.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <h2 class="title">ID：{{id}}</h2>
+    <button @click="gotoPage1"> page1 </button>
+    <button @click="gotoPage2"> page2 </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      id: 0
+    }
+  },
+  onLoad () {
+    console.log('pages/test onLoad')
+    let id = this.$root.$mp.query.id
+    console.log('query', this.$root.$root.$mp.query)
+    this.id = id
+  },
+  onShow () {
+    console.log('pages/test onShow', this.id)
+  },
+  onHide () {
+    console.log('pages/test onHide', this.id)
+  },
+  onUnload () {
+    console.log('pages/test onUnload', this.id)
+  },
+  onReady () {
+    console.log('pages/test onReady', this.id)
+  },
+  beforeCreated () {
+    console.log('pages/test beforeCreated', this.id)
+  },
+  created () {
+    console.log('pages/test created', this.id)
+  },
+  beforeMount () {
+    console.log('pages/test beforeMount', this.id)
+  },
+  mounted () {
+    console.log('pages/test mounted', this.id)
+  },
+  beforeUpdate () {
+    console.log('pages/test beforeUpdate', this.id)
+  },
+  updated () {
+    console.log('pages/test updated', this.id)
+  },
+  beforeDestroy () {
+    console.log('pages/test beforeDestroy', this.id)
+  },
+  destroyed () {
+    console.log('pages/test destroyed', this.id)
+  },
+
+  methods: {
+    gotoPage1 () {
+      wx.navigateTo({
+        url: '/pages/test?id=1'
+      })
+    },
+    gotoPage2 () {
+      wx.navigateTo({
+        url: '/pages/test?id=2'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.title {
+  padding-top: 200px;
+  text-align: center;
+}
+</style>
---
+++ wiki/life-cycle.md
@@ -0,0 +1,23 @@
+page 1
+page 2
+back to page 1
+
+pages/test created 0
+test.vue:17 pages/test onLoad
+test.vue:19 query {id: "1"}
+test.vue:23 pages/test onShow 1
+test.vue:32 pages/test onReady 1
+test.vue:41 pages/test beforeMount 1
+test.vue:44 pages/test mounted 1
+test.vue:26 pages/test onHide 1
+test.vue:17 pages/test onLoad
+test.vue:19 query {id: "2"}
+test.vue:23 pages/test onShow 2
+test.vue:47 pages/test beforeUpdate 2
+test.vue:50 pages/test updated 2
+test.vue:32 pages/test onReady 2
+test.vue:41 pages/test beforeMount 2
+test.vue:47 pages/test beforeUpdate 2
+test.vue:44 pages/test mounted 2
+test.vue:29 pages/test onUnload 2
+test.vue:23 pages/test onShow 2
